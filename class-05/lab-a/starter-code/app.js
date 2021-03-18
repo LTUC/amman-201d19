ef59929b6c8c492974408aff6ca689f3193ba7c6
MURADALSHORMAN <MURADALSHORMAN@GMAIL.COM>
2021-03-18 13:35:06 +0200

    problem2

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -9,13 +9,16 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-
+    let sumNumber=a+b;
+    return([sumNumber,'The sum of ' +a +' and '+ b +' is '+ sumNumber+ '.']);
 }
+    //console.log(sum(4,7));
+
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
+// Once you get the t dest passing,o an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
 /* Problem 2
@@ -27,11 +30,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+    let mulNumber=a*b;
+    return([mulNumber,'The product of ' +a +' and '+ b +' is '+ mulNumber+ '.']);
 }
+    // console.log(multiply(5,9));
+
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
