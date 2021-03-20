d166289cd0b8a915d0879f85a4bc00d32eb80b0f
SUMAYAGHANEM <sumaghanem9@gmail.com>
2021-03-20 10:49:08 -0700

    function2

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -24,19 +24,26 @@ testSum(4, 7);
 
 /////////////////////////////////////
 /* Problem 2
-Write a function called multiply() that takes in two numbers as arguments and returns an array where the first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
+Write a function called multiply() that takes in two numbers as arguments 
+and returns an array where the first element is the product of those numbers,
+ and the second element is a string that EXACTLY follows this example and uses 
+ the values that were input into the function:
 
 "The product of 5 and 9 is 45."
 
-Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
+
+
+Test this function by hand in the console to get it working, and when you think it is finished,
+uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+let product2 = a * b;
+    return [product2, 'The product of '+a+' and '+9 + ' is '+product2+ '.']
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
