60050ac38989f3f2bb05d10c1fe4e0a1e7c1a195
roaaabualeeqa <roaa.abualeeqa@gmail.com>
2021-03-21 12:06:13 +0200

    class-06 demo

---
+++ class-06/demo/js/objects.js
@@ -0,0 +1,21 @@
+'use strict';
+let student2 = ['Ahmad',25,'201d19','ASAC'];
+// console.log(student[1]);
+
+let student = {
+  name:'Ahmad',
+  age:25,
+  course:'201d19',
+  school:'ASAC',
+  likeFootball:true,
+  obj: {id:1,name:'x'}
+};
+let keyName = 'name';
+// console.log(student);
+// console.log(typeof student);
+// console.log(typeof student2);
+// console.log(Array.isArray(student2));
+// console.log(Array.isArray(student));
+// console.log(student.full name);
+// console.log(student['full name']);
+
